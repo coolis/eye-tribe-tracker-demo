dc9cf17c96dba5839c29cfdef4eb84dcc1489365
coolis <wayne.liwen@gmail.com>
2014-10-02 23:08:13 -0400

    little change

--- teamTheia/src/teamTheia/main/Main.java
+++ teamTheia/src/teamTheia/main/Main.java
@@ -1,17 +1,20 @@
 package teamTheia.main;
 
 import com.theeyetribe.client.GazeManager;
+import com.theeyetribe.client.IGazeListener;
 import com.theeyetribe.client.GazeManager.ApiVersion;
 import com.theeyetribe.client.GazeManager.ClientMode;
-import com.theeyetribe.client.IGazeListener;
 import com.theeyetribe.client.data.GazeData;
 
 public class Main
-{
+{	
     public static void main(String[] args)
     {
         final GazeManager gm = GazeManager.getInstance();
         boolean success = gm.activate(ApiVersion.VERSION_1_0, ClientMode.PUSH);
+        final GazeListener gazeListener = new GazeListener();
+        gm.addGazeListener(gazeListener);
+        BoundryCheck bc = new BoundryCheck(gm);
     }
     
     private static class GazeListener implements IGazeListener
@@ -19,7 +22,7 @@ public class Main
         @Override
         public void onGazeUpdate(GazeData gazeData)
         {
-            System.out.println(gazeData.toString());
+            
         }
     }
 }
